70aeea9b16c4049d69b083869c9fb5759a33ee93
jacquespillet <pilletjacq@eisti.eu>
2019-09-02 17:58:42 +0100

    added to shadow

--- src/3DEngine/Shaders/BlinnPhongShader.cpp
+++ src/3DEngine/Shaders/BlinnPhongShader.cpp
@@ -169,7 +169,65 @@ void BlinnPhongParams::SetUniforms() {
 
     int smoothnessLocation = ogl->glGetUniformLocation(this->shader->programShaderObject, "smoothness");
     ogl->glUniform1f(smoothnessLocation, smoothness);
-} 
+}
+
+std::string shadowCalculationFunction = R"(
+    float ShadowCalculation(vec4 fragPosLightSpace, int inx)
+    {
+        vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+        projCoords = projCoords * 0.5 + 0.5;
+        float currentDepth = projCoords.z;
+
+        float bias = max(0.05 * (dot(fragNormal, normalize(lights[inx].direction))), 0.005);  
+        
+        float shadow = 0;
+
+        ///////MANUAL PCF
+        // vec2 texelSize = 1.0 / textureSize(lights[inx].depthMap, 0);
+        // for(int x = -1; x <= 1; ++x)
+        // {
+        //     for(int y = -1; y <= 1; ++y)
+        //     {
+        //         float pcfDepth = texture(lights[inx].depthMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+        //         shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
+        //     }    
+        // }
+        // shadow /= 9.0;
+
+
+        //POISSON DISK SAMPLING
+        vec2 poissonDisk[16] = vec2[]( 
+            vec2( -0.94201624, -0.39906216 ), 
+            vec2( 0.94558609, -0.76890725 ), 
+            vec2( -0.094184101, -0.92938870 ), 
+            vec2( 0.34495938, 0.29387760 ), 
+            vec2( -0.91588581, 0.45771432 ), 
+            vec2( -0.81544232, -0.87912464 ), 
+            vec2( -0.38277543, 0.27676845 ), 
+            vec2( 0.97484398, 0.75648379 ), 
+            vec2( 0.44323325, -0.97511554 ), 
+            vec2( 0.53742981, -0.47373420 ), 
+            vec2( -0.26496911, -0.41893023 ), 
+            vec2( 0.79197514, 0.19090188 ), 
+            vec2( -0.24188840, 0.99706507 ), 
+            vec2( -0.81409955, 0.91437590 ), 
+            vec2( 0.19984126, 0.78641367 ), 
+            vec2( 0.14383161, -0.14100790 ) 
+        );
+        for (int i=0;i<16;i++){
+            int index = int(16.0*randomNumber(vec4(gl_FragCoord.xyy, i)));
+            float depth = texture(lights[inx].depthMap, projCoords.xy + poissonDisk[index]/700.0).r; 
+            if ( depth  <  currentDepth - bias ){
+                shadow+=0.0625;
+            }
+        }
+
+        if(projCoords.z > 1.0)
+            shadow = 0.0;
+
+        return max(0, shadow);
+    } 
+)";
 
 /*
 * Returns the actual blinn phong shader
@@ -254,38 +312,14 @@ Shader GetBlinnPhongShader() {
     in vec2 fragUv;
     in vec4 fragTangent;
     in vec3 toCamTangentSpace; 
-
-    float ShadowCalculation(vec4 fragPosLightSpace, int inx)
-    {
-        // perform perspective divide
-        vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-        // transform to [0,1] range
-        projCoords = projCoords * 0.5 + 0.5;
-        // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
-        // get depth of current fragment from light's perspective
-        float currentDepth = projCoords.z;
-        // check whether current frag pos is in shadow
-
-        float bias = max(0.05 * (dot(fragNormal, normalize(lights[inx].direction))), 0.005);  
-        float shadow = 0.0;
-        vec2 texelSize = 1.0 / textureSize(lights[inx].depthMap, 0);
-        for(int x = -1; x <= 1; ++x)
-        {
-            for(int y = -1; y <= 1; ++y)
-            {
-                float pcfDepth = texture(lights[inx].depthMap, projCoords.xy + vec2(x, y) * texelSize).r; 
-                shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
-            }    
-        }
-        shadow /= 9.0;
-
-
-        if(projCoords.z > 1.0)
-            shadow = 0.0;
-
-        return shadow;
-    } 
-
+    
+    float randomNumber(vec4 seed) {
+        float dot_product = dot(seed, vec4(12.9898,78.233,45.164,94.673));
+        return fract(sin(dot_product) * 43758.5453);
+    }
+    )" +  
+    shadowCalculationFunction
+    + R"(
     void main()
     {
         vec3 fragToCam = cameraPos - fragPos;
@@ -298,7 +332,6 @@ Shader GetBlinnPhongShader() {
         vec4 finalSpecularColor = specularColor;
         if(reflectSkybox == 1) {
             vec3 I = -normalize(fragToCam);
-            
             vec3 R = vec3(0, 0, 0);
             if(finalAlbedo.a < 1) {
                 float ratio = 1.00 / 1.52;
@@ -306,7 +339,6 @@ Shader GetBlinnPhongShader() {
             } else {
                 R = reflect(I, normalize(fragNormal));
             }
-
             finalSpecularColor = vec4(texture(cubemapTexture, R).rgb, 1.0);
         }
 
@@ -339,7 +371,6 @@ Shader GetBlinnPhongShader() {
                 toLight     = normalize(vec3(dot(fragTangent.xyz, toLight),dot(fragBitangent, toLight), dot(fragNormal, toLight)));
                 fragToCam   = normalize(vec3(dot(fragTangent.xyz, fragToCam), dot(fragBitangent, fragToCam), dot(fragNormal, fragToCam)));
             }
-
 
             vec4 diffuse = diffuseFactor * finalAlbedo * lights[i].color * max(dot(finalNormal.xyz, toLight), 0);
 
